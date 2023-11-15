d4bc328cce05efcc1b9fe0767808b7b6ff7d5f89
nunu19nunu <auninwcupcup@gmail.com>
2023-11-15 19:42:21 +0700

    update

--- floodRisk.py
+++ floodRisk.py
@@ -54,10 +54,10 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 
 # Initialize the Random Forest Classifier
-model = RandomForestClassifier(n_estimators=100, random_state=42)  
+model = RandomForestClassifier(n_estimators=100, random_state=42)
 
-# # Train the Random Forest model
-# model.fit(X_train, y_train)
+# Train the Random Forest model
+model.fit(X_train, y_train)
 
 # # Make predictions using the trained Random Forest model
 # predictions = model.predict(X_test)
